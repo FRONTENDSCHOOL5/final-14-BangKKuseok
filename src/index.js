5b54aea8c7b761a890cfbe277f62040eceb6ae35
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-09 05:53:17 +0900

    🎨 style: index.js 코드 포맷팅 및 더미 파일 추가 (#2)
    Co-authored-by: boeunLee <bounjaeun@gmail.com>
    Co-authored-by: jiwon-o <ojw4184@gmail.com>
    Co-authored-by: ony540 <onyy540@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
