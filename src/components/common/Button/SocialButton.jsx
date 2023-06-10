28919d56aa195045b273615a9f1d46db48e19489
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-10 14:56:14 +0900

    🎨 style: 기본 버튼, 소셜 버튼 UI 컴포넌트 구현 (#3)

--- src/components/common/Button/SocialButton.jsx
+++ src/components/common/Button/SocialButton.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import mailIcon from '../../../assets/icons/icon-mail.svg';
+import kakaoIcon from '../../../assets/icons/icon-kakao.svg';
+import googleIcon from '../../../assets/icons/icon-google.svg';
 
-export default function SocialButton() {
-  return <div>SocialButton</div>;
+const SOCIALS = {
+  mail: css`
+    --border: 1px solid ${({ theme }) => theme.colors.mainCoral};
+    --background: url(${mailIcon}) no-repeat left 1em center / 1.7em;
+  `,
+  kakao: css`
+    --border: 1px solid ${({ theme }) => theme.colors.yellow};
+    --background: url(${kakaoIcon}) no-repeat left 1em center / 1.7em;
+  `,
+  google: css`
+    --border: 1px solid ${({ theme }) => theme.colors.gray300};
+    --background: url(${googleIcon}) no-repeat left 1em center / 1.7em;
+  `,
+};
+
+const StyledButton = styled.button`
+  ${(p) => p.social}
+
+  min-width: 322px;
+  padding: 15px 14px;
+  border-radius: 14px;
+  font-weight: 500;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.colors.gray300};
+  border: var(--border, '1px solid currentColor');
+  background: var(--background, none);
+`;
+
+export default function SocialButton({ social, onClick, children }) {
+  const socialStyle = SOCIALS[social];
+
+  return (
+    <StyledButton type='button' social={socialStyle} onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
 }
