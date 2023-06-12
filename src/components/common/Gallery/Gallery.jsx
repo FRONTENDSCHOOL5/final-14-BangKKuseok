42fd8450999b85ae17b2e7ed8f559215582f3a93
boeunLee <bounjaeun@gmail.com>
2023-06-12 15:18:20 +0900

    🎨 style: 갤러리 공통 UI 컴포넌트 구현
    Grid로 레이아웃을 구성하였고, 모바일일때는 2열, 웹일때는 3열로 구현하였다. 사진을 클릭하면 게시물 페이지로 이동하도록 하였다.

--- src/components/common/Gallery/Gallery.jsx
+++ src/components/common/Gallery/Gallery.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export default function Gallery() {
-  return <div>Gallery</div>;
+const GalleryWrapper = styled.div`
+  max-width: 542px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 174px);
+  grid-template-rows: repeat(${(count) => count}, 1fr);
+  gap: 10px;
+`;
+const GalleryBox = styled.div`
+  width: 174px;
+  height: 174px;
+  overflow: hidden;
+`;
+const GalleryImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export default function Gallery({ imgUrl, postId }) {
+  const count = imgUrl.length;
+
+  return (
+    <GalleryWrapper count={count}>
+      {imgUrl.map((url, index) => (
+        <GalleryBox key={index}>
+          <Link to={`/post/${postId}`}>
+            <GalleryImg src={url} alt='no-img' />
+          </Link>
+        </GalleryBox>
+      ))}
+    </GalleryWrapper>
+  );
 }
