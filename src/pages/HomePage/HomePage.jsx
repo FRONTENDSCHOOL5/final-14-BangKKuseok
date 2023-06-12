af3c7d2de42c94f7a291953a5fce99977dcff3f4
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-13 00:32:18 +0900

    🚚 rename: 변수명 및 alt속성명 수정  (#21)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import ProfileImageUpload from '../../components/common/ProfileImageUpload/ProfileImageUpload';
 
 export default function HomePage() {
-  return <div>Home</div>;
+  // return <div>Home</div>;
+  return <div>
+    <ProfileImageUpload/>
+  </div>;
 }
