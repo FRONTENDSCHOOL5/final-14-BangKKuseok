c2a8a6aec1fd5295d6d246741c62589ceda79dd4
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-12 16:28:49 +0900

    🎨 style: Carousel UI 컴포넌트 스타일링 (#9)

--- src/components/common/Carousel/Carousel.jsx
+++ src/components/common/Carousel/Carousel.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import leftIcon from '../../../assets/icons/icon-carousel-left.svg';
+import rightIcon from '../../../assets/icons/icon-carousel-right.svg';
+import {
+  CarouselWrapper,
+  ContentBox,
+  ControlBox,
+  ImageBox,
+  Indicator,
+  PostDate,
+} from './CarouselStyle';
 
-export default function Carousel() {
-  return <div>Carousel</div>;
+export default function Carousel({ data }) {
+  const moveSlide = () => {};
+
+  return (
+    <CarouselWrapper>
+      <ImageBox>
+        {data.map((item) => (
+          <li key={item.id}>
+            <img
+              src={item.image}
+              alt={`${item.author.username}님의  ${JSON.parse(item.content).space}이미지`}
+            />
+            <ContentBox>
+              <label>
+                {item.author.username}님의 {JSON.parse(item.content).space}
+              </label>
+              <PostDate>{item.createdAt.slice(0, 10)}</PostDate>
+              <Indicator>1 / 5</Indicator>
+            </ContentBox>
+          </li>
+        ))}
+      </ImageBox>
+      <ControlBox>
+        <button type='button' aria-label='이전 슬라이드 이동 버튼' onClick={() => moveSlide()}>
+          <img src={leftIcon} alt='' />
+        </button>
+        <button type='button' aria-label='다음 슬라이드 이동 버튼' onClick={() => moveSlide()}>
+          <img src={rightIcon} alt='' />
+        </button>
+      </ControlBox>
+    </CarouselWrapper>
+  );
 }
---
+++ src/components/common/Carousel/CarouselStyle.jsx
@@ -0,0 +1,104 @@
+import styled, { css } from 'styled-components';
+
+const CarouselWrapper = styled.article`
+  width: 390px;
+  height: 404px;
+  position: relative;
+  overflow: hidden;
+`;
+
+const ImageBox = styled.ul`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  li {
+    width: 100%;
+    display: flex;
+    flex-shrink: 0;
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+`;
+
+const ContentBox = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 11px 16px 39px 26px;
+  display: grid;
+  grid-template-areas:
+    'label label'
+    'createdDate indicator';
+  position: absolute;
+  bottom: 0;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(41, 41, 41, 0.87) 89.06%);
+
+  label {
+    grid-area: label;
+    ${({ theme }) => css`
+      color: ${theme.colors.white};
+      font-size: ${theme.fontSize.lg};
+    `}
+    font-weight: 700;
+  }
+`;
+
+const PostDate = styled.span`
+  grid-area: createdDate;
+  align-self: self-end;
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    font-size: ${theme.fontSize.lg};
+  `}
+  font-size: ${({ theme }) => theme.fontSize.xs};
+`;
+
+const Indicator = styled.div`
+  grid-area: indicator;
+  justify-self: end;
+  width: 50px;
+  height: 22px;
+  padding: 5px 0;
+  border-radius: 22px;
+  background: ${({ theme }) => theme.colors.white50};
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.04);
+  backdrop-filter: blur(15px);
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  color: ${({ theme }) => theme.colors.black};
+  text-align: center;
+`;
+
+const ControlBox = styled.div`
+  width: 100%;
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  button {
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.colors.white50};
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.04);
+    backdrop-filter: blur(13px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      width: 5px;
+      height: 10px;
+    }
+  }
+`;
+
+export { CarouselWrapper, ImageBox, ContentBox, PostDate, Indicator, ControlBox };
