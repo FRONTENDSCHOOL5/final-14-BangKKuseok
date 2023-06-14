ecf92d6ddec6b9e0a8daaef37c7dfe5460644a32
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-14 17:51:50 +0900

    ♻️ refactor: Gallery 컴포넌트 props 구조 변경 (#39)
    - 불필요한 props 삭제
    - 변환된 필요 데이터만 받아서 사용할 수 있도록 props 구조 변경

--- src/components/common/Gallery/Gallery.jsx
+++ src/components/common/Gallery/Gallery.jsx
@@ -6,7 +6,6 @@ const GalleryWrapper = styled.div`
   max-width: 542px;
   display: grid;
   grid-template-columns: repeat(auto-fill, 174px);
-  grid-template-rows: repeat(${(count) => count}, 1fr);
   gap: 10px;
 `;
 const GalleryBox = styled.div`
@@ -20,15 +19,13 @@ const GalleryImg = styled.img`
   object-fit: cover;
 `;
 
-export default function Gallery({ imgUrls, postId }) {
-  const count = imgUrls.length;
-
+export default function Gallery({ data }) {
   return (
-    <GalleryWrapper count={count}>
-      {imgUrls.map((url, index) => (
-        <GalleryBox key={index}>
-          <Link to={`/post/${postId}`}>
-            <GalleryImg src={url} alt='no-img' />
+    <GalleryWrapper>
+      {data.map((item) => (
+        <GalleryBox key={item.id}>
+          <Link to={`/post/${item.id}`}>
+            <GalleryImg src={item.imgUrl} alt={`${item.author}님의 공간 이미지`} />
           </Link>
         </GalleryBox>
       ))}
