25d8d700bbda94c588ffab3a4d718a3532fa1c4d
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-06-14 16:26:54 +0900

    ♻️ refactor: Header UI 컴포넌트 리팩토링 (#30)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,65 +1,47 @@
 import React from 'react';
-import {
-  HeaderWrapper,
-  HeaderUI,
-  HeaderShadowUI,
-  SearchBtn,
-  HeaderH2,
-  HeaderH3,
-} from './HeaderStyle';
+import { HeaderWrapper, SearchBtn, HeaderH2, HeaderH3 } from './HeaderStyle';
 import { ReactComponent as BackIcon } from '../../../assets/icons/icon-arrow-left.svg';
 import { ReactComponent as MoreIcon } from '../../../assets/icons/icon-more.svg';
 import { ReactComponent as SearchIcon } from '../../../assets/icons/icon-search.svg';
-import Button from '../Button/Button';
+import Button from '../Button/Button/Button';
 import RoundedInput from '../Input/RoundedInput/RoundedInput';
 
-export default function Header({ type }) {
-  const UI = {
+export default function Header({ type, title, btnText, isBtnActive }) {
+  const HeaderLayout = {
+    home: (
+      <HeaderWrapper type={type}>
+        <SearchBtn>
+          <SearchIcon stroke='#fff' />
+        </SearchBtn>
+      </HeaderWrapper>
+    ),
+    feed: (
+      <HeaderWrapper type={type}>
+        <SearchBtn>
+          <SearchIcon stroke='#000' />
+        </SearchBtn>
+      </HeaderWrapper>
+    ),
+    search: (
+      <HeaderWrapper type={type}>
+        <button>
+          <BackIcon stroke='#000' />
+        </button>
+        <RoundedInput id='search' placeholder='계정을 검색하세요' />
+      </HeaderWrapper>
+    ),
     profile: (
-      <HeaderUI>
+      <HeaderWrapper type={type}>
         <button>
           <BackIcon stroke='#fff' />
         </button>
         <button>
           <MoreIcon fill='#fff' stroke='#fff' />
         </button>
-      </HeaderUI>
-    ),
-    search: (
-      <HeaderShadowUI>
-        <button>
-          <BackIcon stroke='#000' />
-        </button>
-        <RoundedInput id='search' placeholder='계정을 검색하세요' />
-      </HeaderShadowUI>
-    ),
-    feed: (
-      <HeaderUI>
-        <SearchBtn>
-          <SearchIcon stroke='#000' />
-        </SearchBtn>
-      </HeaderUI>
-    ),
-    home: (
-      <HeaderUI>
-        <SearchBtn>
-          <SearchIcon stroke='#fff' />
-        </SearchBtn>
-      </HeaderUI>
-    ),
-    imageSelect: (
-      <HeaderUI>
-        <button>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2>사진 선택</HeaderH2>
-        <Button disabled='true' size='sm'>
-          저장
-        </Button>
-      </HeaderUI>
+      </HeaderWrapper>
     ),
     post: (
-      <HeaderUI>
+      <HeaderWrapper type={type}>
         <button>
           <BackIcon stroke='#000' />
         </button>
@@ -67,8 +49,19 @@ export default function Header({ type }) {
         <button>
           <MoreIcon fill='#000' stroke='#000' />
         </button>
-      </HeaderUI>
+      </HeaderWrapper>
+    ),
+    imageSelect: (
+      <HeaderWrapper type={type}>
+        <button>
+          <BackIcon stroke='#000' />
+        </button>
+        <HeaderH2>{title}</HeaderH2>
+        <Button disabled={!isBtnActive} size='sm'>
+          {btnText}
+        </Button>
+      </HeaderWrapper>
     ),
   };
-  return <HeaderWrapper>{UI[type]}</HeaderWrapper>;
+  return HeaderLayout[type];
 }
--- src/components/common/Header/HeaderStyle.jsx
+++ src/components/common/Header/HeaderStyle.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
 import styled, { css } from 'styled-components';
-import theme from './../../../styles/theme';
 
 const HeaderWrapper = styled.header`
   width: 100%;
-  background-color: transparent;
-`;
-
-const HeaderLayout = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 6rem;
   padding: 0 1.6rem;
-`;
-
-const HeaderUI = styled.div`
-  ${HeaderLayout}
-`;
-
-const HeaderShadowUI = styled.div`
-  ${HeaderLayout}
-  box-shadow: 0 3px 10px rgba(66, 66, 66, 0.04);
+  ${(props) => {
+    switch (props.type) {
+      case 'home':
+        return css`
+          background-color: transparent;
+          position: relative;
+          z-index: 1;
+        `;
+      case 'feed':
+        return css`
+          background-color: ${({ theme }) => theme.colors.white};
+          position: absolute;
+        `;
+      case 'search':
+        return css`
+          background-color: ${({ theme }) => theme.colors.white};
+          box-shadow: 0 3px 10px rgba(66, 66, 66, 0.04);
+        `;
+      case 'profile':
+        return css`
+          background-color: ${({ theme }) => theme.colors.subCoral};
+        `;
+      default:
+        return css`
+          background-color: ${({ theme }) => theme.colors.white};
+        `;
+    }
+  }};
 `;
 
 const HeaderText = css`
   display: inline-block;
   position: absolute;
+  font-weight: 500;
 `;
 
 const HeaderH2 = styled.h2`
@@ -47,4 +61,4 @@ const SearchBtn = styled.button`
   right: 1.6rem;
 `;
 
-export { HeaderWrapper, HeaderUI, HeaderShadowUI, HeaderH2, HeaderH3, SearchBtn };
+export { HeaderWrapper, HeaderH2, HeaderH3, SearchBtn };
