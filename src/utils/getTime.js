e7592fb81ba7077b7223694ca3b25b63c22ec6d5
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-14 10:36:47 +0900

    ♻️ refactor: 날짜 데이터 변경 함수 코드 리팩토링(#25)

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,8 +1,5 @@
 export const convertDateFormat = (date) => {
-  let dateISO = date.replace(/\D/g, ' ').split(' ');
-  dateISO[1]--;
-  const dateUTC = new Date(Date.UTC.apply(null, dateISO));
-
+  const dateUTC = new Date(date)
   const year = dateUTC.getFullYear();
   const month = String(dateUTC.getMonth() + 1).padStart(2, '0');
   const day = String(dateUTC.getDate()).padStart(2, '0');
