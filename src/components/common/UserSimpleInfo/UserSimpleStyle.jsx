adfb31e4baba0ab47d6ba1f6bbbc966f1a269b50
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-06-15 17:42:11 +0900

    🎨 style: UserSimpleInfo 공통 UI 컴포넌트 구현 (#50)

---
+++ src/components/common/UserSimpleInfo/UserSimpleStyle.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const UserSimpleInfoWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const UserInfoBox = styled.div`
+  a {
+    display: flex;
+    align-items: center;
+  }
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+`;
+
+const UserNameBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserName = styled.span`
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  font-weight: 500;
+  margin-bottom: 6px;
+`;
+
+const AccountName = styled.span`
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray300};
+`;
+
+export { UserSimpleInfoWrapper, UserInfoBox, UserNameBox, UserName, AccountName };
