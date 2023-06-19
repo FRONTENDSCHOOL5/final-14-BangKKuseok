c5dd56016822ac916faa6820708a4f212f5e2454
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-19 11:05:26 +0900

    ✨ feat: 프로필 헤더 기능, 상품 상세, 게시글 더보기 모달 연결 (#49)

--- src/components/Profile/PostList/PostList.jsx
+++ src/components/Profile/PostList/PostList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PostCard from '../../common/Card/PostCard/PostCard';
 import Gallery from '../../common/Gallery/Gallery';
+import UserSimpleInfo from '../../common/UserSimpleInfo/UserSimpleInfo';
 import { PostCardList, PostListWrapper } from './PostListStyle';
 import { filterPosts } from '../../../utils/filterPosts';
+import { profile } from '../../../mock/mockData';
 
-export default function PostList({ selectedTab, posts, moreInfo }) {
+export default function PostList({ selectedTab, posts, moreInfo, onClick }) {
   const layout = {
     list: (
       <PostCardList>
         {posts.map((item) => (
           <li key={item._id}>
+            <UserSimpleInfo profile={profile} type={'more'} onClick={onClick} />
             <PostCard data={item} moreInfo={moreInfo} />
           </li>
         ))}
--- src/pages/ProfilePage/MyProfilePage/MyProfilePage.jsx
+++ src/pages/ProfilePage/MyProfilePage/MyProfilePage.jsx
@@ -5,7 +5,12 @@ import ProfileCard from '../../../components/Profile/ProfileCard/ProfileCard';
 import ProductList from '../../../components/Profile/ProductList/ProductList';
 import ViewTabs from '../../../components/common/Tabs/ViewTabs';
 import PostList from '../../../components/Profile/PostList/PostList';
+import BottomSheet from '../../../components/common/BottomSheet/BottomSheet';
+import ListModal from '../../../components/common/BottomSheet/ListModal';
+import BasicModal from '../../../components/common/BottomSheet/BasicModal';
+import ProductDetailCard from '../../../components/Profile/ProductDetailCard/ProductDetailCard';
 import { posts, products, profile } from '../../../mock/mockData';
+import { useNavigate } from 'react-router-dom';
 
 const ProfilePageWrapper = styled.main``;
 
@@ -19,24 +24,74 @@ const Message = styled.p`
 
 export default function MyProfile() {
   const [selectedTab, setSelectedTab] = useState('list');
+  const [selectedProduct, setSelectedProduct] = useState('');
+  const [isShowMoreProfile, setIsShowMoreProfile] = useState(false);
+  const [isShowMorePost, setIsShowMorePost] = useState(false);
+  const [isShowProductDetail, setIsShowProductDetail] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleClickTabButton = (tabId) => {
     setSelectedTab(tabId);
   };
 
+  const handleClickMoreProfileButton = () => {
+    setIsShowMoreProfile(true);
+  };
+
+  const handleClickMorePostButton = () => {
+    setIsShowMorePost(true);
+  };
+
+  const handleClickProduct = (productId) => {
+    if (!isShowProductDetail) {
+      const selectedProductInfo = products.filter((product) => product.id === productId);
+      setSelectedProduct(selectedProductInfo);
+    }
+    setIsShowProductDetail(true);
+  };
+
+  const handleClickCloseModal = () => {
+    setIsShowMoreProfile(false);
+    setIsShowMorePost(false);
+    setIsShowProductDetail(false);
+  };
+
   return (
-    <BasicLayout type={'profile'}>
+    <BasicLayout
+      type={'profile'}
+      onClickLeftButton={() => navigate(-1)}
+      onClickRightButton={handleClickMoreProfileButton}
+    >
       <ProfilePageWrapper>
         <ProfileCard profile={profile} />
-        <ProductList products={products} />
-        {posts.length > 1 ? (
+        <ProductList products={products} onClick={handleClickProduct} />
+        {posts.length > 0 ? (
           <>
             <ViewTabs selectedTab={selectedTab} onClick={handleClickTabButton} />
-            <PostList selectedTab={selectedTab} posts={posts} moreInfo={false} />
+            <PostList
+              selectedTab={selectedTab}
+              posts={posts}
+              moreInfo={false}
+              onClick={handleClickMorePostButton}
+            />
           </>
         ) : (
           <Message>작성된 게시물이 없습니다.</Message>
         )}
+
+        {/* -- BottomSheet */}
+        <BottomSheet isShow={isShowMoreProfile} onClick={handleClickCloseModal}>
+          <ListModal type='profile' />
+        </BottomSheet>
+        <BottomSheet isShow={isShowMorePost} onClick={handleClickCloseModal}>
+          <ListModal type='myPost' />
+        </BottomSheet>
+        <BottomSheet isShow={isShowProductDetail} onClick={handleClickCloseModal}>
+          <BasicModal>
+            <ProductDetailCard selectedProduct={selectedProduct} />
+          </BasicModal>
+        </BottomSheet>
       </ProfilePageWrapper>
     </BasicLayout>
   );
