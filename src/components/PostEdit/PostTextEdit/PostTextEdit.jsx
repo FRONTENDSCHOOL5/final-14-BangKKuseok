14521a829b588e72bf623fb3e4bf5140ab552977
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-20 11:01:26 +0900

    ✨ feat: 게시글 수정 페이지 api 연동 및 기능 구현  (#82)

--- src/components/PostEdit/PostTextEdit/PostTextEdit.jsx
+++ src/components/PostEdit/PostTextEdit/PostTextEdit.jsx
@@ -3,6 +3,17 @@ import { SelectSpaceBtn } from '../../PostUpload/PostTextWrite/PostTextWriteStyl
 import { PostTextEditWrapper } from './PostTextEditStyle';
 
 export default function PostTextEdit({ onClick, setContent, content, setEditData, editData }) {
+  //인풋의 변화감지해서 해당밸류넣기
+  const handleChangeDetail = useCallback(
+    (e) => {
+      setContent({ ...content, detail: e.target.value });
+      setEditData({
+        ...editData,
+        content: JSON.stringify({ ...content, detail: e.target.value }),
+      });
+    },
+    [content, editData, setContent, setEditData],
+  );
 
   return (
     <PostTextEditWrapper>
--- src/pages/PostPage/PostEditPage/PostEditPage.jsx
+++ src/pages/PostPage/PostEditPage/PostEditPage.jsx
@@ -17,6 +17,112 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { uploadImg } from '../../../api/imgApi';
 
 export default function PostEditPage() {
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const [isBtnActive, setIsBtnActive] = useState(false);
+  const [isShow, setIsShow] = useState(false);
+
+  const [prevData, setPrevData] = useState();
+  const [editData, setEditData] = useState({
+    content: '',
+    image: '',
+  });
+  const [postImg, setPostImg] = useState();
+  const [content, setContent] = useState({ space: '', detail: '' });
+
+  //게시글 상세 정보받기
+  const { data: postData, isLoading: isPostLoading } = useQuery(
+    ['postData', postId],
+    () => getPostDetail(postId),
+    {
+      onSuccess: (data) => {
+        const { space, detail } = JSON.parse(data.post.content);
+        setPostImg(data.post.image);
+        setContent({ space, detail });
+        setPrevData({ image: data.post.image, content: JSON.stringify({ space, detail }) });
+        setEditData({ image: data.post.image, content: JSON.stringify({ space, detail }) });
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    },
+  );
+
+  //이미지 업로드하기
+  const uploadImgMutation = useMutation(uploadImg, {
+    onSuccess(data) {
+      const imgUrl = URL + data.filename;
+      setPostImg(imgUrl);
+      setEditData({ ...editData, image: imgUrl });
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  const queryClient = useQueryClient();
+  //게시글 수정하기
+  const EditPostMutation = useMutation(editPost, {
+    onSuccess(data) {
+      queryClient.setQueryData(['postData', postId], data);
+      navigate(`/post/${postId}`);
+    },
+    onError(error) {
+      console.log(error);
+    },
+  });
+
+  //이미지 바꿨을 때 이미지 업로드 및 수정
+  const handleEditPostImg = (e) => {
+    const imgData = new FormData();
+    imgData.append('image', e.target.files[0]);
+    uploadImgMutation.mutate(imgData);
+    setIsBtnActive(true);
+  };
+
+  //공간목록 바텀시트 여닫기
+  const handleClickModalOpen = () => {
+    setIsShow((prev) => !prev);
+  };
+
+  //공간 선택하기
+  const handleClickSpace = (e) => {
+    setContent({ ...content, space: e.target.innerText });
+    setEditData({
+      ...editData,
+      content: JSON.stringify({ ...content, space: e.target.innerText }),
+    });
+    setIsShow(false);
+  };
+
+  //뒤로가기 버튼 누르기
+  const handleClickLeftButton = () => {
+    navigate(-1);
+  };
+
+  //게시글 수정 버튼 누르기
+  const handleClickRightButton = () => {
+    EditPostMutation.mutate({
+      postId,
+      post: { ...editData },
+    });
+  };
+
+  //editData 동기적 작업
+  useEffect(() => {
+    setEditData((data) => data);
+  }, [editData]);
+
+  //data에 변동이 있을 때 버튼 활성화
+  useEffect(() => {
+    if (editData && prevData) {
+      if (editData !== prevData) {
+        setIsBtnActive(true);
+      } else {
+        setIsBtnActive(false);
+      }
+    }
+  }, [setIsBtnActive, prevData, editData, content]);
 
   return (
     <>
