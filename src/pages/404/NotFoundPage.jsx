a940f0c26fbbd92cddeac4ac1cf8050cce535b3d
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-23 01:36:49 +0900

    🎨 style: 404페이지 마크업 (#107)

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NoneFeedWrapper } from '../FeedPage/FeedPageStyle';
+import notFoundImg from '../../assets/images/404.png';
+import Button from '../../components/common/Button/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../../components/common/Navbar/Navbar';
+
+const NoneDataWrapper = styled(NoneFeedWrapper)`
+  & > img {
+    width: 240px;
+    margin-bottom: 6px;
+  }
+`;
 
 export default function NotFoundPage() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleClickBackButton = () => {
+    navigate(-1);
+  };
+  return (
+    <NoneDataWrapper>
+      <img src={notFoundImg} alt='404 이미지' />
+      <p>페이지를 찾을 수 없습니다 :(</p>
+      <Button size='md' onClick={handleClickBackButton}>
+        이전 페이지
+      </Button>
+      <Navbar />
+    </NoneDataWrapper>
+  );
 }
