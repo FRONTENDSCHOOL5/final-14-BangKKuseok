64923896f4c2b9cb2ad8740e78cc824d5b093d80
boeunLee <bounjaeun@gmail.com>
2023-06-24 00:27:00 +0900

    ♻️ refactor: 로그인, 회원가입 불필요한 코드 삭제 및 수정, 변수 이름 수정 (#66)
    불필요한 코드를 삭제 및 수정하였고, 버튼 활성화 상태를 관리하던 isInValid보다 btnDisabled라는 변수명이 의미있다고 판단하여 수정하였음. 또한, 로그인 부분에서 onBlur되었을 시에 이메일이나 비밀번호 값이 빈칸이라면 에러메시지를 출력하도록 수정하였음.

--- src/components/LoginSignUp/SignUp/SignUp.jsx
+++ src/components/LoginSignUp/SignUp/SignUp.jsx
@@ -19,22 +19,24 @@ const SignUp = ({ setStep, setPreData, preData }) => {
   // 이메일이 유효하지 않으면 true
   const [isEmailInValid, setIsEmailInValid] = useState(true);
   // 비밀번호가 유효하지 않으면 true
-  const [isPwInValid, setIsPwInValid] = useState(true);
+  const [isPasswordInValid, setIsPasswordInValid] = useState(false);
 
-  // 버튼 활성화를 위해 만듦. 사용자가 입력하는 동안은 버튼 활성화가 되지만, 버튼을 눌렀을 때 이메일 혹은 비밀번호가 유효하지 않으면 값을 true로 바꿔줌으로써 버튼을 disabled 시킴
-  const [isInValid, setIsInValid] = useState(false);
+  // 버튼 활성화를 위해 만듦.
+  const [btnDisabled, setBtnDisabled] = useState(false);
 
   const signupMutation = useMutation(checkEmailExist, {
     onSuccess: (formData) => {
       if (formData.message === '사용 가능한 이메일 입니다.') {
         setIsEmailInValid(false);
-        if (!isEmailInValid && !isPwInValid) {
+        if (!isEmailInValid && !isPasswordInValid) {
           setPreData({ email: emailValue, password: passwordValue });
           setStep('프로필설정');
         }
       } else if (formData.message === '이미 가입된 이메일 주소 입니다.') {
         setEmailError(formData.message);
         setIsEmailInValid(true);
+        setBtnDisabled(true);
+        console.log('dd', btnDisabled);
       }
     },
     onError: (error) => {
@@ -45,11 +47,7 @@ const SignUp = ({ setStep, setPreData, preData }) => {
   // 이메일 입력값 받아오기
   const handleEmailChange = (e) => {
     const value = e.target.value;
-    // 잘못된 이메일을 입력하고 커서만 올려놓고 값이 변하지 않았을때는 if문 실행 안함
-    if (value !== emailValue) {
-      setEmailError('');
-      setIsInValid(false);
-    }
+    setEmailError('');
     setEmailValue(value);
   };
 
@@ -57,63 +55,55 @@ const SignUp = ({ setStep, setPreData, preData }) => {
   const handlePasswordChange = (e) => {
     const value = e.target.value;
     setPasswordError('');
-    setIsInValid(false);
     setPasswordValue(value);
   };
 
-  // 비밀번호 입력값이 바뀔때마다
   useEffect(() => {
-    // 비밀번호 길이가 6보다 작다면
-    if (passwordValue.length < 6) {
-      setIsPwInValid(true);
-    } else {
-      setIsPwInValid(false);
-    }
-  }, [passwordValue]);
+    setBtnDisabled(false);
+  }, [emailValue, passwordValue]);
 
   // 이메일 유효성 검사 후 에러메시지 갱신
   const handleValidateEmail = (e) => {
-    if (e.type === 'blur') {
-      if (
-        !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i.test(emailValue) &&
-        emailValue.length > 0
-      ) {
-        setIsEmailInValid(true);
-        setEmailError('잘못된 이메일 형식입니다.');
-      } else if (emailValue === '') {
-        setIsEmailInValid(true);
-        setEmailError('이메일을 입력해주세요.');
-      } else if (emailError === '이미 가입된 이메일 주소 입니다.') {
-        setIsEmailInValid(true);
-      } else {
-        setEmailError('');
-        setIsEmailInValid(false);
-      }
+    if (
+      !/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i.test(emailValue) &&
+      emailValue.length > 0
+    ) {
+      setIsEmailInValid(true);
+      setEmailError('잘못된 이메일 형식입니다.');
+    } else if (emailValue.length === 0) {
+      setIsEmailInValid(true);
+      setEmailError('이메일을 입력해주세요.');
+    } else {
+      setEmailError('');
+      setIsEmailInValid(false);
     }
   };
 
   // 비밀번호 유효성 검사 후 에러메시지 갱신
   const handleValidatePassword = () => {
-    if (isPwInValid) {
-      if (passwordValue === '') {
-        setPasswordError('비밀번호를 입력해주세요.');
-      } else {
-        setPasswordError('비밀번호는 6자 이상이어야 합니다.');
-      }
+    if (passwordValue.length === 0) {
+      setIsPasswordInValid(true);
+      setPasswordError('비밀번호를 입력해주세요.');
+    } else if (passwordValue.length < 6 && passwordValue.length > 0) {
+      setIsPasswordInValid(true);
+      setPasswordError('비밀번호는 6자 이상이어야 합니다.');
     } else {
       setPasswordError('');
-      setIsPwInValid(false);
+      setIsPasswordInValid(false);
     }
   };
 
   // 버튼이 눌리면
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleValidatePassword();
-    signupMutation.mutate({ user: { email: emailValue } });
 
-    // 이메일, 비밀번호 둘 중 유효하지 않은 값이 있다면 isInValid가 true
-    setIsInValid(isEmailInValid || isPwInValid);
+    handleValidateEmail();
+    handleValidatePassword();
+
+    setBtnDisabled(isEmailInValid || isPasswordInValid);
+    if (!btnDisabled) {
+      signupMutation.mutate({ user: { email: emailValue } });
+    }
   };
   return (
     <form>
@@ -141,16 +131,11 @@ const SignUp = ({ setStep, setPreData, preData }) => {
           warningMsg={passwordError}
           onChange={handlePasswordChange}
           onBlur={handleValidatePassword}
-          isInValid={isPwInValid}
+          isInValid={isPasswordInValid}
         />
 
         <SignUpBottomBox>
-          <Button
-            type='submit'
-            size='lg'
-            onClick={handleSubmit}
-            disabled={emailValue === '' || passwordValue === '' || isInValid}
-          >
+          <Button type='submit' size='lg' onClick={handleSubmit} disabled={btnDisabled}>
             다음
           </Button>
           <SignUpGoBack>
