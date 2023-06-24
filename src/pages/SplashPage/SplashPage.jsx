4f145e26995910b78ba9267a7f642975f739af0c
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-24 15:52:23 +0900

    🚚 rename: splash 이미지 파일 추가 및 warning 처리(#114)

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -10,7 +10,7 @@ export default function SplashPage() {
   useEffect(() => {
     const timer = setTimeout(() => navigate('/main'), 2500);
     return () => clearTimeout(timer);
-  }, []);
+  }, [navigate]);
 
   return (
     <SplashPageWrapper>
