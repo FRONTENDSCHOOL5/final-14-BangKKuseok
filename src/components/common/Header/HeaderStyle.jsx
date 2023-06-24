d95fdcc97af43aca826aaca6dbe5de034e443029
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-24 16:48:19 +0900

    🎨 style: 헤더 스타일 수정 (#113)

--- src/components/common/Header/HeaderStyle.jsx
+++ src/components/common/Header/HeaderStyle.jsx
@@ -18,7 +18,7 @@ const HeaderWrapper = styled.header`
       case 'feed':
         return css`
           background-color: ${({ theme }) => theme.colors.white};
-          position: fixed;
+          position: absolute;
           z-index: 1;
         `;
       case 'search':
