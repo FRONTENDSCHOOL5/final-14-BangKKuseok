051d1740b235712802c1b66b1ab1a91067e60c6c
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-06-25 20:01:09 +0900

    ✨ feat: 검색 API 연동 (#117)

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import BasicLayout from '../../layout/BasicLayout';
 import UserSimpleInfo from '../../components/common/UserSimpleInfo/UserSimpleInfo/UserSimpleInfo';
-import { profile } from '../../mock/mockData';
 import styled from 'styled-components';
+import { useQuery } from 'react-query';
+import { getSearchResult } from '../../api/searchApi';
 import Skeleton from '../../components/common/UserSimpleInfo/Skeleton/Skeleton';
 
 const UserInfoList = styled.ul`
@@ -13,29 +14,45 @@ const UserInfoList = styled.ul`
   }
 `;
 
+const Message = styled.p`
+  font-weight: 500;
+  font-size: ${({ theme }) => theme.fontSize.base};
+  color: ${({ theme }) => theme.colors.gray200};
+  padding-top: 52px;
+  text-align: center;
+`;
+
 export default function Search({ onClickLeftButton }) {
-  const [search, setSearch] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const { data: searchResult, isLoading } = useQuery(
+    ['searchUser', inputValue],
+    () => getSearchResult(inputValue),
+    {
+      enabled: !!inputValue,
+      select: (result) =>
+        result.filter((user) => {
+          return user.username.includes(inputValue);
+        }),
+    },
+  );
 
   const handleChangeInput = (e) => {
-    setSearch(e.target.value);
+    setInputValue(e.target.value);
   };
-
-  const filterProfile = profile.filter((p) => {
-    return p.username.toLowerCase().includes(search.toLowerCase());
-  });
 
   return (
     <BasicLayout
       type='search'
       onClickLeftButton={onClickLeftButton}
-      value={search}
+      value={inputValue}
       onChange={handleChangeInput}
     >
       <UserInfoList>
-        {search &&
-          (filterProfile.length >= 1 ? (
+        {inputValue &&
+          (searchResult?.length >= 1 ? (
             <>
-              {filterProfile.map((profile) => (
+              {searchResult.map((profile) => (
                 <li key={profile._id}>
                   <UserSimpleInfo profile={profile} isLink={true} />
                 </li>
@@ -43,11 +60,13 @@ export default function Search({ onClickLeftButton }) {
             </>
           ) : (
             <>
-              {profile.map((profile) => (
-                <li key={profile._id}>
-                  <Skeleton />
-                </li>
-              ))}
+              {Array(10)
+                .fill()
+                .map((_, idx) => (
+                  <li key={idx}>
+                    <Skeleton />
+                  </li>
+                ))}
             </>
           ))}
       </UserInfoList>
