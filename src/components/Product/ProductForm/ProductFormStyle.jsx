656e77e0e5d44272b0038789d6e14a88ea0ed7d7
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-06-25 22:55:06 +0900

    ✨ feat: 상품 등록, 수정 페이지에서 사용될 폼 컴포넌트 마크업 및 기능 구현 (#91)

---
+++ src/components/Product/ProductForm/ProductFormStyle.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ProductFormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 18px 0;
+
+  div:nth-child(1) {
+    margin-bottom: 30px;
+  }
+`;
