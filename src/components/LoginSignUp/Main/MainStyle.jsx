bd003e64fa4c5b1b61d873ee8849fd72acb58f16
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-26 11:34:50 +0900

    ✨ feat: 메인페이지 소파 모델 인터랙션 기능 추가 (#110)

--- src/components/LoginSignUp/Main/MainStyle.jsx
+++ src/components/LoginSignUp/Main/MainStyle.jsx
@@ -2,34 +2,55 @@ import styled from 'styled-components';
 
 export const MainPageWrapper = styled.main`
   width: clamp(390px, 100%, 720px);
-  min-height: 100vh;
+  height: 100vh;
   margin: 0 auto;
-  background-color: ${({ theme }) => theme.colors.mainCoral};
+  background-color: ${({ theme }) => theme.colors.subCoral};
+  text-align: center;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: space-between;
 `;
 
 export const WhiteLogoImg = styled.img`
-  object-fit: cover;
   width: 187px;
-
-  margin: 223px auto 200px;
+  max-width: calc(100% - 102px);
+  margin: 107px auto 0;
+  display: inline-block;
 `;
 
-export const BottomBox = styled.section`
-  height: 362px;
+export const CanvasBox = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 170px;
+  min-width: 150px;
+  aspect-ratio: 1;
+  & > div {
+    z-index: 1;
+  }
+  canvas {
+    filter: saturate(1.5);
+  }
+  img {
+    position: absolute;
+    top: -8px;
+    left: -8px;
+    width: 202px;
+    min-width: 182px;
+  }
+`;
+
+export const BottomSection = styled.section`
   border-radius: 20px 20px 0 0;
   background-color: ${({ theme }) => theme.colors.white};
-  display: flex;
-  flex-direction: column;
+  padding: 4vh 0;
 `;
+
 export const BtnBox = styled.div`
-  align-items: center;
   display: flex;
+  align-items: center;
   flex-direction: column;
+  justify-content: flex-start;
   gap: 10px;
-  margin-top: 50px;
 `;
 
 export const AccountSignUpBox = styled.span`
@@ -38,7 +59,7 @@ export const AccountSignUpBox = styled.span`
   color: ${({ theme }) => theme.colors.gray300};
   margin-top: 2px;
 
-  & > *:not(:last-child)::after {
+  & > *:first-child::after {
     content: ' | ';
   }
 `;
