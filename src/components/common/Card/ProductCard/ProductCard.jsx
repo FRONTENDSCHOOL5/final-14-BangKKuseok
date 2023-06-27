f883bba519ad18240048b6b9eafece86db8bc0dc
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-06-27 15:18:06 +0900

    ♻️ refactor: 상품카드 이미지 없는 경우 예외 처리 (#142)

--- src/components/common/Card/ProductCard/ProductCard.jsx
+++ src/components/common/Card/ProductCard/ProductCard.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Keyword, ProductCardWrapper, ProductInfoBox } from './ProductCardStyle';
+import basicImage from '../../../../assets/images/profile.png';
 
 export default function ProductCard({ data, onClick }) {
   const { itemName, price, itemImage } = data;
@@ -7,9 +8,18 @@ export default function ProductCard({ data, onClick }) {
     ? JSON.parse(itemName)
     : { name: undefined, keyword: undefined };
 
+  const [error, setError] = useState(false);
+  const handleImageError = () => {
+    setError(true);
+  };
+
   return (
     <ProductCardWrapper onClick={onClick}>
-      <img src={itemImage} alt={name ? name : itemName} />
+      {!error ? (
+        <img src={itemImage} alt={name ? name : itemName} onError={handleImageError} />
+      ) : (
+        <img src={basicImage} alt='기본 이미지' />
+      )}
       <ProductInfoBox>
         <h3>{name ? name : itemName}</h3>
         <p>{price.toLocaleString()}원</p>
