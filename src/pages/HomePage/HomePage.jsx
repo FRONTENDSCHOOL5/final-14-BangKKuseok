3b557de6a49b1e89918c3362ca6b767cf343d338
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-06-27 16:18:34 +0900

    ✨ feat: FeedApi와 캐러셀 연동 및 좋아요순 정렬 (#127)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,6 +11,8 @@ import styled from 'styled-components';
 import Gallery from '../../components/common/Gallery/Gallery';
 import { filterPosts } from '../../utils/filterPosts';
 import Spinner from '../../components/common/Spinner/Spinner';
+import { topLikedPosts } from '../../mock/mockData';
+import { getFeedPost } from '../../api/feedApi';
 
 const Message = styled.p`
   font-weight: 500;
@@ -22,7 +24,7 @@ const Message = styled.p`
 
 const TabWrapper = styled.div`
   border-radius: 16px 16px 0 0;
-  margin-top: -16px;
+  margin-top: -24px;
   position: sticky;
   top: 0;
   z-index: 1;
@@ -41,39 +43,75 @@ export default function HomePage() {
   const [filteredPosts, setfilteredPosts] = useState([]);
   const [allPosts, setAllPosts] = useState([]);
   const [isTabClick, setIsTabClick] = useState(false);
-  const [sortedPosts, setSortedPosts] = useState([]);
 
   const count = useRef(0);
   const [isLast, setIsLast] = useState(false);
 
+  // 캐러셀 팔로잉 게시글 목록 불러오기
   const {
-    data: postData,
-    isLoading,
-    fetchNextPage,
-    isFetchingNextPage,
-    hasNextPage,
-  } = useInfiniteQuery('posts', ({ skip = count.current }) => getAllPost({ limit: 100, skip }), {
-    getNextPageParam: (lastPage) => {
-      return lastPage.nextPage + 1;
+    data: feedPostData,
+    fetchNextPage: feedFetchNextPage,
+    isLoading: feedIsLoading,
+    isFetching: feedIsFetching,
+    hasNextPage: feedHasNextPage,
+  } = useInfiniteQuery(
+    'feedPostData',
+    ({ pageParam = { skip: 0 } }) => getFeedPost({ skip: pageParam.skip }),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length > 0 ? allPages.length * 10 : 0;
+        return lastPage.data.length < 10 ? undefined : { skip: nextPage };
+      },
+      select: (data) => {
+        return data.pages
+          .flatMap((page) => page.data)
+          .filter((post) => post.content?.includes('"space"'))
+          .sort((a, b) => b.heartCount - a.heartCount)
+          .slice(0, 5);
+      },
+      refetchOnWindowFocus: true,
     },
-    onSuccess: (newData) => {
-      const pageParam = newData.pageParams.length - 1;
-      setAllPosts([...newData.pages[pageParam].data]);
-      if (newData.pages[pageParam].isLast) {
-        setIsLast(true);
-      }
+  );
 
-      const allFiltered = allPosts.filter((post) => post.content?.includes('"space"'));
-      setFilteredAllPosts((prevData) => [...prevData, ...allFiltered]);
+  const {
+    data: homePostData,
+    isLoading: homeIsLoading,
+    isFetching: homeIsFetching,
+    fetchNextPage: homeFetchNextPage,
+  } = useInfiniteQuery(
+    'homePostData',
+    ({ skip = count.current }) => getAllPost({ limit: 100, skip }),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.nextPage + 1;
+      },
+      onSuccess: (newData) => {
+        const pageParam = newData.pageParams.length - 1;
+        setAllPosts([...newData.pages[pageParam].data]);
+        if (newData.pages[pageParam].isLast) {
+          setIsLast(true);
+        }
+
+        const allFiltered = allPosts.filter((post) => post.content?.includes('"space"'));
+        setFilteredAllPosts((prevData) => [...prevData, ...allFiltered]);
+      },
     },
-  });
+  );
 
+  // 홈 피드 100개씩 불러오기
   useEffect(() => {
     if (!isLast) {
-      fetchNextPage();
+      homeFetchNextPage();
       count.current += 100;
     }
-  }, [postData, isLoading, isLast, fetchNextPage]);
+  }, [homePostData, homeIsLoading, isLast, homeFetchNextPage]);
+
+  // 캐러셀 필터링
+  useEffect(() => {
+    if (feedHasNextPage) {
+      feedFetchNextPage();
+    }
+  }, [feedHasNextPage]);
 
   const handleClickTabButton = (e) => {
     const index = ['전체', ...SPACES].indexOf(e.target.innerText);
@@ -99,21 +137,15 @@ export default function HomePage() {
     setIsClickSearchButton(true);
   };
 
-  if (hasNextPage && isLoading && isFetchingNextPage)
-    return (
-      <BasicLayout>
-        <span>로딩중..</span>
-      </BasicLayout>
-    );
+  if (homeIsLoading || feedIsLoading || feedIsFetching) return <Spinner />;
 
   return (
     <>
-      {(isLoading || filteredAllPosts.length === 0) && <Spinner />}
       {isClickSearchButton ? (
         <Search onClickLeftButton={handleClickLeftButton} />
       ) : (
         <BasicLayout type='home' onClickRightButton={handleClickRightButton}>
-          <Carousel data={sortedPosts} />
+          <Carousel data={feedPostData?.length > 0 ? feedPostData : topLikedPosts} />
           <TabWrapper>
             <SpaceTabs
               currentTab={currentTab}
