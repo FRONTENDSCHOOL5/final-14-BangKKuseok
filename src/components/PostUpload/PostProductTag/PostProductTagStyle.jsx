fad8d3a8df54b438a7c6f59a92abadd31bfacef0
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-07-13 00:34:26 +0900

    ♻️ refactor: 클릭뿐아니라 드래그로도 태그 가능하도록 수정 (#208)

--- src/components/PostUpload/PostProductTag/PostProductTagStyle.jsx
+++ src/components/PostUpload/PostProductTag/PostProductTagStyle.jsx
@@ -37,16 +37,16 @@ const TagBox = styled.ul`
     `}
 `;
 
-  & > img {
-    position: absolute;
-    width: 20px;
-    border-radius: 50%;
-    box-shadow: 0px 0px 3px rgba(62, 62, 62, 0.3);
-    transform: translate(-50%, -50%);
-    left: 50%;
-    top: 50%;
-    z-index: 1;
-  }
+const GuideTagButton = styled.div.attrs(({ addTagBtn, mouseLoc }) => ({
+  style: {
+    background: `url(${addTagBtn}) no-repeat ${mouseLoc.x}% ${mouseLoc.y}% / 20px`,
+  },
+}))`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0px 0px 3px rgba(62, 62, 62, 0.3);
+  z-index: 1;
 `;
 
 const bouncing = keyframes`
@@ -63,18 +63,21 @@ const bouncing = keyframes`
 }
 100% {
   transform: translate(-50%, -50%) scale(2.3);
-  opacity: 0.4;
 }
 `;
 
-const BouncingCircle = styled.div`
+const BouncingCircle = styled.div.attrs(({ mouseLoc }) => ({
+  style: {
+    left: mouseLoc.x + '%',
+    top: mouseLoc.y + '%',
+  },
+}))`
   position: absolute;
   border-radius: 50%;
   width: 20px;
   height: 20px;
   background-color: white;
   animation: ${bouncing} 1s linear alternate infinite;
-  left: 50%;
-  top: 50%;
 `;
-export { PostProductTagWrapper, ImgBox, TagBox, BouncingCircle };
+
+export { PostProductTagWrapper, ImgBox, TagBox, BouncingCircle, GuideTagButton };
