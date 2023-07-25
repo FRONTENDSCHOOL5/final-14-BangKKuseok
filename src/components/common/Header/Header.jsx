e443c2e8ed626c861004cd039c02cd07ef0f6311
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-07-25 19:07:02 +0900

    ♻️ refactor: 헤더 코드 간소화 및 리팩토링 (#234)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderWrapper, SearchBtn, HeaderH2, HeaderH3, HeaderSpan } from './HeaderStyle';
+import { HeaderWrapper, SearchBtn, HeaderH2, HeaderSpan } from './HeaderStyle';
 import { ReactComponent as BackIcon } from '../../../assets/icons/icon-arrow-left.svg';
 import { ReactComponent as MoreIcon } from '../../../assets/icons/icon-more.svg';
 import { ReactComponent as SearchIcon } from '../../../assets/icons/icon-search.svg';
@@ -19,94 +19,97 @@ export default function Header({
   onChange,
   onClickTitle,
 }) {
+  let leftButton;
+  let centerContent;
+  let rightButton;
+
   const navigate = useNavigate();
 
-  const HeaderLayout = {
-    home: (
-      <HeaderWrapper type={type}>
-        <SearchBtn onClick={onClickRightButton}>
-          <SearchIcon stroke='#fff' />
-        </SearchBtn>
-      </HeaderWrapper>
-    ),
-    feed: (
-      <HeaderWrapper type={type}>
-        <button onClick={() => navigate(-1)}>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2>{title}</HeaderH2>
-        <SearchBtn type='button' onClick={onClickRightButton}>
-          <SearchIcon stroke='#000' />
-        </SearchBtn>
-      </HeaderWrapper>
-    ),
-    search: (
-      <HeaderWrapper type={type}>
-        <button onClick={onClickLeftButton}>
-          <BackIcon stroke='#000' />
-        </button>
+  const backButton = (
+    <button onClick={type === 'search' ? onClickLeftButton : () => navigate(-1)}>
+      <BackIcon stroke={type === 'profile' ? '#fff' : '#000'} />
+    </button>
+  );
+
+  const searchButton = (
+    <SearchBtn onClick={onClickRightButton}>
+      <SearchIcon stroke={type === 'home' ? '#fff' : '#000'} />
+    </SearchBtn>
+  );
+
+  const moreButton = (
+    <button type='button' onClick={onClickRightButton}>
+      <MoreIcon
+        fill={type === 'profile' ? '#fff' : '#000'}
+        stroke={type === 'profile' ? '#fff' : '#000'}
+      />
+    </button>
+  );
+
+  const saveButton = (
+    <Button disabled={!isBtnActive} size='sm' onClick={onClickRightButton}>
+      {btnText}
+    </Button>
+  );
+
+  const headerTitle = (
+    <HeaderH2 onClick={type === 'post' ? onClickTitle : null}>
+      {title}
+      {type === 'post' ? <HeaderSpan>{subtitle}</HeaderSpan> : null}
+    </HeaderH2>
+  );
+
+  switch (type) {
+    case 'home':
+      rightButton = searchButton;
+      break;
+    case 'feed':
+      leftButton = backButton;
+      centerContent = <HeaderH2>{title}</HeaderH2>;
+      rightButton = searchButton;
+      break;
+    case 'search':
+      leftButton = backButton;
+      centerContent = (
         <RoundedInput
           id='search'
           placeholder='계정을 검색하세요'
           value={value}
           onChange={onChange}
         />
-      </HeaderWrapper>
-    ),
-    profile: (
-      <HeaderWrapper type={type}>
-        <button type='button' onClick={() => navigate(-1)}>
-          <BackIcon stroke='#fff' />
-        </button>
-        <button type='button' onClick={onClickRightButton}>
-          <MoreIcon fill='#fff' stroke='#fff' />
-        </button>
-      </HeaderWrapper>
-    ),
-    profileEdit: (
-      <HeaderWrapper type={type}>
-        <button type='button' onClick={() => navigate(-1)}>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2>{title}</HeaderH2>
-        <Button disabled={!isBtnActive} size='sm' onClick={onClickRightButton}>
-          {btnText}
-        </Button>
-      </HeaderWrapper>
-    ),
-    follow: (
-      <HeaderWrapper type={type}>
-        <button type='button' onClick={() => navigate(-1)}>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2>{title}</HeaderH2>
-      </HeaderWrapper>
-    ),
-    post: (
-      <HeaderWrapper type={type}>
-        <button type='button' onClick={() => navigate(-1)}>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2 onClick={onClickTitle}>
-          {title}
-          <HeaderSpan>{subtitle}</HeaderSpan>
-        </HeaderH2>
-        <button type='button' onClick={onClickRightButton}>
-          <MoreIcon fill='#000' stroke='#000' />
-        </button>
-      </HeaderWrapper>
-    ),
-    imageSelect: (
-      <HeaderWrapper type={type}>
-        <button type='button' onClick={() => navigate(-1)}>
-          <BackIcon stroke='#000' />
-        </button>
-        <HeaderH2>{title}</HeaderH2>
-        <Button disabled={!isBtnActive} size='sm' onClick={onClickRightButton}>
-          {btnText}
-        </Button>
-      </HeaderWrapper>
-    ),
-  };
-  return HeaderLayout[type];
+      );
+      break;
+    case 'profile':
+      leftButton = backButton;
+      rightButton = moreButton;
+      break;
+    case 'profileEdit':
+      leftButton = backButton;
+      centerContent = headerTitle;
+      rightButton = saveButton;
+      break;
+    case 'follow':
+      leftButton = backButton;
+      centerContent = headerTitle;
+      break;
+    case 'post':
+      leftButton = backButton;
+      centerContent = headerTitle;
+      rightButton = moreButton;
+      break;
+    case 'imageSelect':
+      leftButton = backButton;
+      centerContent = headerTitle;
+      rightButton = saveButton;
+      break;
+    default:
+      return null;
+  }
+  return (
+    <HeaderWrapper type={type}>
+      {leftButton}
+      {centerContent}
+      {rightButton}
+    </HeaderWrapper>
+  );
 }
