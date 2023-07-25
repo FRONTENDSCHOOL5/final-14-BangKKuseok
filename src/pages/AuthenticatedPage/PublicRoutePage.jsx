61d4f40d556b16889ac086b6e678dff0587e4403
boeunLee <bounjaeun@gmail.com>
2023-07-25 18:41:39 +0900

    ♻️ refactor: 프로그램 실행시 로그인이 되어있는 상태에서 alert창이 뜨는 현상 해결 (#217)

--- src/pages/AuthenticatedPage/PublicRoutePage.jsx
+++ src/pages/AuthenticatedPage/PublicRoutePage.jsx
@@ -2,9 +2,21 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PublicRoutePage = () => {
   const authenticated = localStorage.getItem('token');
+  const visited = sessionStorage.getItem('hasVisited');
+
+  // 로그인 상태에서 최초 방문시 경고창 띄우지 않도록 한다.
+  if (!visited) {
+    sessionStorage.setItem('hasVisited', true);
+  }
 
   return authenticated ? (
-    <Navigate {...window.alert('잘못된 접근입니다. 로그아웃 후 시도해 주세요.')} to='/home' />
+    // 로그인 상태라면 PublicRoutePage 접근 방지
+    visited ? (
+      <Navigate to='/home' {...alert('로그아웃 이후 접근 가능한 페이지입니다.')} />
+    ) : (
+      // 최초 접근시 경고창 띄우지 않음
+      <Navigate to='/home' />
+    )
   ) : (
     <Outlet />
   );
