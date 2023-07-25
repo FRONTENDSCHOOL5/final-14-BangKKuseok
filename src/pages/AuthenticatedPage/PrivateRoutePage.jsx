710c7a62d004747a09099318675b4044d169683b
boeunLee <bounjaeun@gmail.com>
2023-07-25 18:47:18 +0900

    💬 comment: 주석 추가 (#217)
    PrivateRoutePage의 주석을 추가하였다.

--- src/pages/AuthenticatedPage/PrivateRoutePage.jsx
+++ src/pages/AuthenticatedPage/PrivateRoutePage.jsx
@@ -4,6 +4,7 @@ const PrivateRoutePage = () => {
   const authenticated = localStorage.getItem('token');
 
   return !authenticated ? (
+    // 로그아웃 상태라면 PrivateRoutePage 접근 방지
     <Navigate to='/' {...alert('로그인 이후 접근 가능한 페이지입니다.')} />
   ) : (
     <Outlet />
