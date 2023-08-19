5626cb78ccec0b55ef6ac06373125c63cdba2eae
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-08-19 16:10:27 +0900

    ♻️ refactor: 헤더 코드 리팩토링 (#234)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -7,6 +7,51 @@ import Button from '../Button/Button/Button';
 import RoundedInput from '../Input/RoundedInput/RoundedInput';
 import { useNavigate } from 'react-router-dom';
 
+function BackButton({ type, onClick }) {
+  const navigate = useNavigate();
+  return (
+    <button onClick={type === 'search' ? onClick : () => navigate(-1)}>
+      <BackIcon stroke={type === 'profile' ? '#fff' : '#000'} />
+    </button>
+  );
+}
+
+function SearchButton({ type, onClick }) {
+  return (
+    <SearchBtn onClick={onClick}>
+      <SearchIcon stroke={type === 'home' ? '#fff' : '#000'} />
+    </SearchBtn>
+  );
+}
+
+function MoreButton({ type, onClick }) {
+  return (
+    <button type='button' onClick={onClick}>
+      <MoreIcon
+        fill={type === 'profile' ? '#fff' : '#000'}
+        stroke={type === 'profile' ? '#fff' : '#000'}
+      />
+    </button>
+  );
+}
+
+function SaveButton({ onClick, btnText, isBtnActive }) {
+  return (
+    <Button disabled={!isBtnActive} size='sm' onClick={onClick}>
+      {btnText}
+    </Button>
+  );
+}
+
+function HeaderTitle({ type, onClickTitle, title, subtitle }) {
+  return (
+    <HeaderH2 onClick={type === 'post' ? onClickTitle : null}>
+      {title}
+      {type === 'post' ? <HeaderSpan>{subtitle}</HeaderSpan> : null}
+    </HeaderH2>
+  );
+}
+
 export default function Header({
   type,
   title,
@@ -19,97 +64,72 @@ export default function Header({
   onChange,
   onClickTitle,
 }) {
-  let leftButton;
-  let centerContent;
-  let rightButton;
-
-  const navigate = useNavigate();
-
-  const backButton = (
-    <button onClick={type === 'search' ? onClickLeftButton : () => navigate(-1)}>
-      <BackIcon stroke={type === 'profile' ? '#fff' : '#000'} />
-    </button>
-  );
-
-  const searchButton = (
-    <SearchBtn onClick={onClickRightButton}>
-      <SearchIcon stroke={type === 'home' ? '#fff' : '#000'} />
-    </SearchBtn>
-  );
-
-  const moreButton = (
-    <button type='button' onClick={onClickRightButton}>
-      <MoreIcon
-        fill={type === 'profile' ? '#fff' : '#000'}
-        stroke={type === 'profile' ? '#fff' : '#000'}
-      />
-    </button>
-  );
-
-  const saveButton = (
-    <Button disabled={!isBtnActive} size='sm' onClick={onClickRightButton}>
-      {btnText}
-    </Button>
-  );
-
-  const headerTitle = (
-    <HeaderH2 onClick={type === 'post' ? onClickTitle : null}>
-      {title}
-      {type === 'post' ? <HeaderSpan>{subtitle}</HeaderSpan> : null}
-    </HeaderH2>
-  );
-
   switch (type) {
     case 'home':
-      rightButton = searchButton;
-      break;
-    case 'feed':
-      leftButton = backButton;
-      centerContent = <HeaderH2>{title}</HeaderH2>;
-      rightButton = searchButton;
-      break;
-    case 'search':
-      leftButton = backButton;
-      centerContent = (
-        <RoundedInput
-          id='search'
-          placeholder='계정을 검색하세요'
-          value={value}
-          onChange={onChange}
-        />
+      return (
+        <HeaderWrapper type={type}>
+          <SearchButton type={type} onClick={onClickRightButton} />
+        </HeaderWrapper>
       );
-      break;
+    case 'feed':
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <HeaderTitle type={type} onClickTitle={onClickTitle} title={title} />
+          <SearchButton type={type} onClick={onClickRightButton} />
+        </HeaderWrapper>
+      );
+    case 'search':
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <RoundedInput
+            id='search'
+            placeholder='계정을 검색하세요'
+            value={value}
+            onChange={onChange}
+          />
+        </HeaderWrapper>
+      );
     case 'profile':
-      leftButton = backButton;
-      rightButton = moreButton;
-      break;
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <MoreButton type={type} onClick={onClickRightButton} />
+        </HeaderWrapper>
+      );
     case 'profileEdit':
-      leftButton = backButton;
-      centerContent = headerTitle;
-      rightButton = saveButton;
-      break;
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <HeaderTitle type={type} onClickTitle={onClickTitle} title={title} />
+          <SaveButton onClick={onClickRightButton} btnText={btnText} isBtnActive={isBtnActive} />
+        </HeaderWrapper>
+      );
     case 'follow':
-      leftButton = backButton;
-      centerContent = headerTitle;
-      break;
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <HeaderTitle type={type} onClickTitle={onClickTitle} title={title} />
+        </HeaderWrapper>
+      );
     case 'post':
-      leftButton = backButton;
-      centerContent = headerTitle;
-      rightButton = moreButton;
-      break;
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <HeaderTitle type={type} onClickTitle={onClickTitle} title={title} subtitle={subtitle} />
+          <MoreButton type={type} onClick={onClickRightButton} />
+        </HeaderWrapper>
+      );
     case 'imageSelect':
-      leftButton = backButton;
-      centerContent = headerTitle;
-      rightButton = saveButton;
-      break;
+      return (
+        <HeaderWrapper type={type}>
+          <BackButton type={type} onClick={onClickLeftButton} />
+          <HeaderTitle type={type} onClickTitle={onClickTitle} title={title} />
+          <SaveButton onClick={onClickRightButton} btnText={btnText} isBtnActive={isBtnActive} />
+        </HeaderWrapper>
+      );
     default:
       return null;
   }
-  return (
-    <HeaderWrapper type={type}>
-      {leftButton}
-      {centerContent}
-      {rightButton}
-    </HeaderWrapper>
-  );
 }
